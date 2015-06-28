6aa2328cba4893ac8abfdf8255a3b441f4e8da2e
cah-viralbhagat <viral.bhagat@cardinalhealth.com>
2015-06-28 15:24:18 -0400

    added argument

--- file-watcher.js
+++ file-watcher.js
@@ -1,5 +1,11 @@
-const fs = require('fs');
-fs.watch('watch.txt', function(){
-    console.log('watch.txt updated');
+const
+    fs = require('fs'),
+    filename = process.argv[2];
+
+if(!filename){
+    throw Error("A file to watch must be specified!");
+}
+fs.watch(filename, function(){
+    console.log("File " + filename + " just changed!");
 });
-console.log('watching watch.txt');
+console.log("watching " + filename);

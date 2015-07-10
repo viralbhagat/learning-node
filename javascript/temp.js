64acc336d5390b4235159d0a97e6947bfd507624
cah-viralbhagat <viral.bhagat@cardinalhealth.com>
2015-07-10 15:34:25 -0400

    added new files

--- javascript/temp.js
+++ javascript/temp.js
@@ -1,19 +1,8 @@
-
-function Person(name) {
-    Object.defineProperty(this, "name", {
-        get: function() {
-            return name;
-        },
-        set: function(newName) {
-            name = newName;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    this.sayName = function() {
-        console.log(this.name);
-    };
-} // false
-
-var person1 = new Person("Viral");
-person1.sayName();
+var http = require('http');
+http.createServer(function(request, response){
+    response.write("<html>");
+    response.write("<head><title>Node.js JS</title></head>");
+    response.write("<body>Hello Web</body>");
+    response.write("</html>");
+    response.end();
+}).listen(9999);
